1f9c38052c88490d8f8789d02eff5373bf9ffa6b
Johanna L <johannarleal@gmail.com>
2021-01-19 10:01:45 -0500

    Added new function addAnswerRows to js

--- script.js
+++ script.js
@@ -1,52 +1,83 @@
+var btnStartQuiz = document.getElementById("start-quiz-btn");
 
-function initializeQuiz() {
+var answersDiv = document.getElementById("answers");
 
-    var quizQAs = {
-        question : null,
-        answer1 : null,
-        answer2 : null,
-        answer3 : null,
-        answer4 : null,
-        correctAnswer : 0
-    };
+// Question and answers object array
+var questionsAndAnswers = [];
 
-    quizQAs.question = "Inside which HTML element do we put the JavaScript?";
-    quizQAs.answer1 = "<javascript>";
-    quizQAs.answer2 = "<js>";
-    quizQAs.answer3 = "<scripting>";
-    quizQAs.answer4 = "<script>";
-    quizQAs.correctAnswer = 4;
+btnStartQuiz.addEventListener("click", function() {
+    // If the Start Quiz button was clicked, initialize the 
+    // quiz questions and answers object array
+    initializeQuestionAndAnswerArray();
 
-    var questionsAndAnswers = [quizQAs];
+    // Add answer rows to answers column.
+    addAnswerRows();
 
-    quizQAs.question = "What is the correct place to insert a JavaScript?";
-    quizQAs.answer1 = "The <body> section";
-    quizQAs.answer2 = "The <head> section";
-    quizQAs.answer3 = "The <inner> section";
-    quizQAs.answer4 = "Both the <head> and the <body> section";
-    quizQAs.correctAnswer = 1;
+    // Display the first question and related answers.
+    displayQandA(0);
 
-    questionsAndAnswers.push(quizQAs);
+});
 
-    quizQAs.question = "What is the correct syntax for referring to an external script called xxx.js?";
-    quizQAs.answer1 = "<script name='xxx.js'";
-    quizQAs.answer2 = "<script href='xxx.js'";
-    quizQAs.answer3 = "<script src='xxx.js'";
-    quizQAs.answer4 = ",script alt='xxx.js'";
-    quizQAs.correctAnswer = 3;
+// initializeQuestionAndAnswerArray initializes a global array of objects that contains each question, 
+// its related answers, and the correct answer number.
+function initializeQuestionAndAnswerArray() {
+    // Add question 1, answer set, and correct answer to the global array
+    questionsAndAnswers = [
+        {
+            question: "Inside which HTML element do we put the JavaScript?",
+            answer1: "<javascript>",
+            answer2: "<js>",
+            answer3: "<scripting>",
+            answer4: "<script>",
+            correctAnswer: 4
+        }
+    ];
 
-    questionsAndAnswers.push(quizQAs);
+    // Add question 2, answer set, and correct answer to the global array
+    questionsAndAnswers.push({
+        question: "What is the correct place to insert a JavaScript?",
+        answer1: "The <body> section",
+        answer2: "The <head> section",
+        answer3: "The <inner> section",
+        answer4: "Both the <head> and the <body> section",
+        correctAnswer: 1
+    });
 
-    quizQAs.question = "How do you write 'Hello World' in an alert box?";
-    quizQAs.answer1 = "msg('Hello World');";
-    quizQAs.answer2 = "msgBox('Hello World');";
-    quizQAs.answer3 = "alert('Hello World');";
-    quizQAs.answer4 = "alertBox('Hello World');";
-    quizQAs.correctAnswer = 3;
+    // Add question 3, answer set, and correct answer to the global array
+    questionsAndAnswers.push({
+        question: "What is the correct syntax for referring to an external script called xxx.js?",
+        answer1: "<script name='xxx.js'",
+        answer2: "<script href='xxx.js'",
+        answer3: "<script src='xxx.js'",
+        answer4: ",script alt='xxx.js'",
+        correctAnswer: 3
+    });
 
-    questionsAndAnswers.push(quizQAs);
-
-    document.getElementById("question").textContent = questionsAndAnswers[0].question;
+    // Add question 4, answer set, and correct answer to the global array
+    questionsAndAnswers.push({
+        question: "How do you write 'Hello World' in an alert box?",
+        answer1: "msg('Hello World');",
+        answer2: "msgBox('Hello World');",
+        answer3: "alert('Hello World');",
+        answer4: "alertBox('Hello World');",
+        correctAnswer: 3
+    });
 }
 
-initializeQuiz();
+function displayQandA(questionNumber) {
+
+    document.getElementById("question").innerText = questionsAndAnswers[questionNumber].question;
+
+}
+
+function addAnswerRows() {
+
+    // answersDiv.removeChild(answersDiv.getElementsByTagName("<p>"));
+    var node = document.createElement("div");
+    node.className = "row";
+
+    answersDiv.appendChild(node);
+
+    node.className = "col-lg-4";
+    answersDiv.appendChild(node);
+}
