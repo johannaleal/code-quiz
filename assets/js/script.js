1d27dc241c4954f41e54a687ad7fab5a61db26b2
Johanna L <johannarleal@gmail.com>
2021-01-19 22:22:16 -0500

    Added code for next question. Added comments.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,46 @@
-var btnStartQuiz = document.getElementById("start-quiz-btn");
-
-var answersDiv = document.getElementById("answers");
+// DECLARATIONS
 
 // Question and answers object array
 var questionsAndAnswers = [];
+
+// Set current question.
+var currentQuestion;
+
+// Correct answer of current question.
+var correctAnswer = 0;
+
+// Total score
+var score = 0;
+
+// Store Start Quiz button object.
+var btnStartQuiz = document.getElementById("start-quiz-btn");
+
+// Get answers div object and store.
+var answersDiv = document.getElementById("answers");
+
+// This will check if one of the answers is clicked in the answers div.
+answersDiv.addEventListener("click", function(event) {
+    var element = event.target;
+    var userAnswer;
+
+    if (element.matches("div")) {
+        // Get the answer number that the user clicked.
+        userAnswer = Number(element.getAttribute("data-number"));
+
+        // If the user's selected answer matches the correct answer
+        // for the question, display a message stating that they won.
+        if (userAnswer === correctAnswer ) {
+            document.getElementById("message").innerText = "Correct!";
+        }
+        else {
+            document.getElementById("message").innerText = "Wrong!";
+        }
+
+        // Display the next question.
+        currentQuestion++;
+        displayQandA(currentQuestion);
+    };
+});
 
 btnStartQuiz.addEventListener("click", function() {
     // If the Start Quiz button was clicked do the following:
@@ -18,7 +55,11 @@ btnStartQuiz.addEventListener("click", function() {
     btnStartQuiz.remove();
 
     // Display the first question and related answers.
-    displayQandA(0);
+    currentQuestion = 0;
+    displayQandA(currentQuestion);
+    //correctAnswer = getCorrectAnswer(currentQuestion);
+
+    //checkForAnswerClicked();
 
 });
 
@@ -66,13 +107,17 @@ function initializeQuestionAndAnswerArray() {
         "answer4": "alertBox('Hello World');",
         correctAnswer: 3
     });
-}
+};
 
 function setTimer() {
 
-}
+};
 
+// This function will display the question and related answers using the the question
+// number passed in as a parameter.
 function displayQandA(questionNumber) {
+    // Clear any messages from previous question.
+    //document.getElementById("message").innerText = "";
 
     // Get the question from the question and answer array amd display it in the question div.
     document.getElementById("question").innerText = questionsAndAnswers[questionNumber].question;
@@ -82,16 +127,37 @@ function displayQandA(questionNumber) {
         document.getElementById("answer" + i.toString()).innerText = i.toString() + ". " + questionsAndAnswers[questionNumber]["answer" + i.toString()];
     };
 
-}
+    // Get the correct answer number for this question and store it
+    // in a global variable.
+    correctAnswer = getCorrectAnswer(currentQuestion);
+};
 
-function addAnswerRows() {
+// Get the correct answer from the questions and answers array for the
+// question number passed in as a parameter.
+function getCorrectAnswer(answerNum) {
 
-    // answersDiv.removeChild(answersDiv.getElementsByTagName("<p>"));
-    var node = document.createElement("div");
-    node.className = "row";
+    return questionsAndAnswers[answerNum].correctAnswer;
 
-    answersDiv.appendChild(node);
+};
 
-    node.className = "col-lg-4";
-    answersDiv.appendChild(node);
-}
+// function checkForAnswerClicked() {
+//     answersDiv.addEventListener("click", function(event) {
+//         var element = event.target;
+
+//         if (element.matches("div")) {
+//             console.log(element.innerText);
+//         }
+//     });
+// }
+
+// function addAnswerRows() {
+
+//     // answersDiv.removeChild(answersDiv.getElementsByTagName("<p>"));
+//     var node = document.createElement("div");
+//     node.className = "row";
+
+//     answersDiv.appendChild(node);
+
+//     node.className = "col-lg-4";
+//     answersDiv.appendChild(node);
+// }
